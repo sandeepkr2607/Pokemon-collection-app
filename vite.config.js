e33e368da8b6a18122e6d300b642375084fb7c27
sandeepkr2607 <sandeepkr2607@gmail.com>
2025-08-07 19:45:12 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,6 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // eslint-disable-next-line no-undef
+  base: process.env.VITE_ENV_PATH || '/Pokemon-collection-app',
 });
